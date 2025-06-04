5200c9c06f349f43c825b674d3787e37fef04555
Haoran Wu <haoran.wu@outlook.com>
2025-06-04 13:59:33 +0800

    feat: update model imports to include OminiFlux transformer and pipeline

--- nunchaku/pipeline/__init__.py
+++ nunchaku/pipeline/__init__.py
@@ -1,3 +1,4 @@
 from .pipeline_flux_pulid import PuLIDFluxPipeline
+from .pipeline_omini_flux import OminiFluxSubPixelPipeline
 
-__all__ = ["PuLIDFluxPipeline"]
+__all__ = ["PuLIDFluxPipeline", "OminiFluxSubPixelPipeline"]
